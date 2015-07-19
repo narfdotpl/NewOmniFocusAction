555bca32f570e6c715bcd705e3e34520a3abb903
Maciej Konieczny <hello@narf.pl>
2015-07-19 10:32:56 +0200

    added single screen project

---
+++ New OmniFocus Action/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  New OmniFocus Action
+//
+//  Created by Maciej Konieczny on 2015-07-19.
+//  Copyright (c) 2015 Maciej Konieczny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
